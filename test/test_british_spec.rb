6ef1092d574424ed06af04d5580e1977befc0f7f
Serge Bedzhyk <smileart21@gmail.com>
2019-11-20 22:26:02 +0200

    fix: resolve yard/inch related security issue chore: update all dev dependencies chore: patch version bump 0.4.1 → 0.4.2 test: update minitest test (deprecated syntax)
    Signed-off-by: Serge Bedzhyk <smileart21@gmail.com>

--- test/test_british_spec.rb
+++ test/test_british_spec.rb
@@ -11,55 +11,55 @@ describe 'British module' do
   describe 'When included to the British class:' do
     it '#new must call initialize (initialise)' do
       british_object = BritishObject.new('Hello world!')
-      british_object.test.must_equal 'Hello world!'
+      _(british_object.test).must_equal 'Hello world!'
     end
 
     it '#new must call inherited initialize' do
       british_object = PoshHash.new
       h = Hash.new
 
-      british_object.default.must_equal PoshHash::DEFAULT
-      british_object[:nonsense].must_equal PoshHash::DEFAULT
+      _(british_object.default).must_equal PoshHash::DEFAULT
+      _(british_object[:nonsense]).must_equal PoshHash::DEFAULT
 
-      british_object.color.must_equal 'crimson'
+      _(british_object.color).must_equal 'crimson'
 
-      h[:nonsense].must_be_nil
+      _(h[:nonsense]).must_be_nil
     end
 
     it 'must correctly process simple names' do
       british_object = BritishObject.new('Hello world!')
 
-      british_object.magnetize('magnet').must_equal 'magnet'
-      british_object.surprize!.must_equal 'Surprize!'
-      british_object.surprize?.must_equal 'Surprize?'
+      _(british_object.magnetize('magnet')).must_equal 'magnet'
+      _(british_object.surprize!).must_equal 'Surprize!'
+      _(british_object.surprize?).must_equal 'Surprize?'
 
       british_object.surprize = 'Surprize!!!'
-      british_object.surprize.must_equal 'Surprize!!!'
+      _(british_object.surprize).must_equal 'Surprize!!!'
     end
 
     it 'must correctly process complicated names' do
       british_object = BritishObject.new('Hello world!')
-      british_object.initialize_something('test').must_equal 'test'
+      _(british_object.initialize_something('test')).must_equal 'test'
     end
 
     it 'mustn\'t brake native American methods' do
       british_object = BritishObject.new('Hello world!')
-      british_object.magnetize('magnet').must_equal 'magnet'
+      _(british_object.magnetize('magnet')).must_equal 'magnet'
     end
 
     it 'must process different endings (not -ise only)' do
       british_object = BritishObject.new('Hello world!')
-      british_object.color.must_equal 'red'
+      _(british_object.color).must_equal 'red'
     end
 
     it 'must implement is_an? (is_a? alias)' do
       british_object = BritishObject.new('Hello world!')
-      british_object.is_an?(Array).must_equal false
+      _(british_object.is_an?(Array)).must_equal false
     end
 
     it 'must implement an? (is_an?/is_a? alias)' do
       british_object = BritishObject.new('Hello world!')
-      british_object.an?(Array).must_equal false
+      _(british_object.an?(Array)).must_equal false
     end
 
     it 'mustn\'t break host 3rd party object when included' do
@@ -68,15 +68,15 @@ describe 'British module' do
         include British
       end
 
-      'test'.capitalize.must_equal 'Test'
-      'test'.capitalise.must_equal 'Test'
+      _('test'.capitalize).must_equal 'Test'
+      _('test'.capitalise).must_equal 'Test'
     end
 
     it 'must work with the inheritance' do
       my = MyClass.new('foo')
-      my.a.must_equal 'bar'
-      my.b.must_equal 'foo'
-      my.z.must_equal 'Alice & Bob'
+      _(my.a).must_equal 'bar'
+      _(my.b).must_equal 'foo'
+      _(my.z).must_equal 'Alice & Bob'
     end
 
     it 'mustn\'t be broken by other\'s magic' do
@@ -84,19 +84,19 @@ describe 'British module' do
       real_magic = RealMagicClass.new
       witchcraft = InheritedWitchcraft.new
 
-      magic.rabbit_color.must_equal 'white'
-      magic.perform.must_equal 'A rabbit from a top hat!'
+      _(magic.rabbit_color).must_equal 'white'
+      _(magic.perform).must_equal 'A rabbit from a top hat!'
 
-      real_magic.rabbit_color.must_equal 'white'
-      real_magic.perform.must_equal 'Abracadabra! A rabbit from a top hat!'
+      _(real_magic.rabbit_color).must_equal 'white'
+      _(real_magic.perform).must_equal 'Abracadabra! A rabbit from a top hat!'
 
-      witchcraft.rabbit_color.must_equal 'white'
-      witchcraft.cat_color.must_equal 'black'
-      witchcraft.perform.must_equal 'Oo ee oo ah ah ting tang walla walla bing bang!'
+      _(witchcraft.rabbit_color).must_equal 'white'
+      _(witchcraft.cat_color).must_equal 'black'
+      _(witchcraft.perform).must_equal 'Oo ee oo ah ah ting tang walla walla bing bang!'
     end
 
     it 'must throw NoMethodError on unknown methods' do
-      -> { BigBadGrayWolf.new.color }.must_raise NoMethodError
+      _(-> { BigBadGrayWolf.new.color }).must_raise NoMethodError
     end
 
     it 'must correctly show nil in NoMethodError messages' do
@@ -105,9 +105,9 @@ describe 'British module' do
         nil.nonsense
       rescue NoMethodError => e
         if RUBY_VERSION == '2.0.0'
-          e.message.must_equal 'private method `include\' called for NilClass:Class'
+          _(e.message).must_equal 'private method `include\' called for NilClass:Class'
         else
-          e.message.must_equal 'undefined method `nonsense\' for nil:NilClass'
+          _(e.message).must_equal 'undefined method `nonsense\' for nil:NilClass'
         end
       end
     end
@@ -116,55 +116,55 @@ describe 'British module' do
   describe 'When included to the American class:' do
     it '#new must call initialize' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.test.must_equal 'Hello world!'
+      _(british_object.test).must_equal 'Hello world!'
     end
 
     it '#new must call inherited initialize' do
       british_object = PoshHash.new
       h = Hash.new
 
-      british_object.default.must_equal PoshHash::DEFAULT
-      british_object[:nonsense].must_equal PoshHash::DEFAULT
+      _(british_object.default).must_equal PoshHash::DEFAULT
+      _(british_object[:nonsense]).must_equal PoshHash::DEFAULT
 
-      british_object.colour.must_equal 'crimson'
+      _(british_object.colour).must_equal 'crimson'
 
-      h[:nonsense].must_be_nil
+      _(h[:nonsense]).must_be_nil
     end
 
     it 'must correctly process simple names' do
       british_object = AmericanObject.new('Hello world!')
 
-      british_object.magnetise('magnet').must_equal 'magnet'
-      british_object.surprise!.must_equal 'Surprize!'
-      british_object.surprise?.must_equal 'Surprize?'
+      _(british_object.magnetise('magnet')).must_equal 'magnet'
+      _(british_object.surprise!).must_equal 'Surprize!'
+      _(british_object.surprise?).must_equal 'Surprize?'
 
       british_object.surprise = 'Surprize!!!'
-      british_object.surprise.must_equal 'Surprize!!!'
+      _(british_object.surprise).must_equal 'Surprize!!!'
     end
 
     it 'must correctly process complicated names' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.initialise_something('test').must_equal 'test'
+      _(british_object.initialise_something('test')).must_equal 'test'
     end
 
     it 'mustn\'t brake native American methods' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.magnetise('magnet').must_equal 'magnet'
+      _(british_object.magnetise('magnet')).must_equal 'magnet'
     end
 
     it 'must process different endings (not -ise only)' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.color.must_equal 'red'
+      _(british_object.color).must_equal 'red'
     end
 
     it 'must implement is_an? (is_a? alias)' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.is_an?(Array).must_equal false
+      _(british_object.is_an?(Array)).must_equal false
     end
 
     it 'must implement an? (is_an?/is_a? alias)' do
       british_object = AmericanObject.new('Hello world!')
-      british_object.an?(Array).must_equal false
+      _(british_object.an?(Array)).must_equal false
     end
 
     it 'must affect host 3rd party object when included' do
@@ -173,14 +173,14 @@ describe 'British module' do
         include British
       end
 
-      'test'.capitalise.must_equal 'Test'
+      _('test'.capitalise).must_equal 'Test'
     end
 
     it 'must work with the inheritance' do
       my = MyClass.new('foo')
-      my.a.must_equal 'bar'
-      my.b.must_equal 'foo'
-      my.z.must_equal 'Alice & Bob'
+      _(my.a).must_equal 'bar'
+      _(my.b).must_equal 'foo'
+      _(my.z).must_equal 'Alice & Bob'
     end
 
     it 'mustn\'t be broken by other\'s magic' do
@@ -188,19 +188,19 @@ describe 'British module' do
       real_magic = RealMagicClass.new
       witchcraft = InheritedWitchcraft.new
 
-      magic.rabbit_colour.must_equal 'white'
-      magic.perform.must_equal 'A rabbit from a top hat!'
+      _(magic.rabbit_colour).must_equal 'white'
+      _(magic.perform).must_equal 'A rabbit from a top hat!'
 
-      real_magic.rabbit_colour.must_equal 'white'
-      real_magic.perform.must_equal 'Abracadabra! A rabbit from a top hat!'
+      _(real_magic.rabbit_colour).must_equal 'white'
+      _(real_magic.perform).must_equal 'Abracadabra! A rabbit from a top hat!'
 
-      witchcraft.rabbit_colour.must_equal 'white'
-      witchcraft.cat_colour.must_equal 'black'
-      witchcraft.perform.must_equal 'Oo ee oo ah ah ting tang walla walla bing bang!'
+      _(witchcraft.rabbit_colour).must_equal 'white'
+      _(witchcraft.cat_colour).must_equal 'black'
+      _(witchcraft.perform).must_equal 'Oo ee oo ah ah ting tang walla walla bing bang!'
     end
 
     it 'must throw NoMethodError on unknown methods' do
-      -> { BigBadGrayWolf.new.colour }.must_raise NoMethodError
+      _(-> { BigBadGrayWolf.new.colour }).must_raise NoMethodError
     end
 
     it 'must correctly show nil in NoMethodError messages' do
@@ -209,9 +209,9 @@ describe 'British module' do
         nil.nonsense
       rescue NoMethodError => e
         if RUBY_VERSION == '2.0.0'
-          e.message.must_equal 'private method `include\' called for NilClass:Class'
+          _(e.message).must_equal 'private method `include\' called for NilClass:Class'
         else
-          e.message.must_equal 'undefined method `nonsense\' for nil:NilClass'
+          _(e.message).must_equal 'undefined method `nonsense\' for nil:NilClass'
         end
       end
     end
@@ -222,61 +222,61 @@ describe 'British module' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.magnetise('magnet').must_equal 'magnet'
-      freeloader.surprise!.must_equal 'Surprise!'
-      freeloader.surprise?.must_equal 'Surprise?'
+      _(freeloader.magnetise('magnet')).must_equal 'magnet'
+      _(freeloader.surprise!).must_equal 'Surprise!'
+      _(freeloader.surprise?).must_equal 'Surprise?'
 
       freeloader.surprise = 'Surprise!!!'
-      freeloader.surprise.must_equal 'Surprise!!!'
+      _(freeloader.surprise).must_equal 'Surprise!!!'
     end
 
     it 'must correctly process complicated names' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.initialise_something('test').must_equal 'test'
+      _(freeloader.initialise_something('test')).must_equal 'test'
     end
 
     it 'mustn\'t brake native American methods' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.magnetize('magnet').must_equal 'magnet'
+      _(freeloader.magnetize('magnet')).must_equal 'magnet'
     end
 
     it 'must process different endings (not -ise only)' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.colour.must_equal 'red'
+      _(freeloader.colour).must_equal 'red'
     end
 
     it 'must implement is_an? (is_a? alias)' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.is_an?(Array).must_equal false
+      _(freeloader.is_an?(Array)).must_equal false
     end
 
     it 'must implement an? (is_an?/is_a? alias)' do
       freeloader = Freeloader.new('foo', 'Hello world!')
       freeloader.extend(British)
 
-      freeloader.an?(Array).must_equal false
+      _(freeloader.an?(Array)).must_equal false
     end
 
     it 'must patch 3rd party object when used in #extend' do
       freeloader = Freeloader.new('foo', 'Hello World!')
       freeloader.extend(British)
 
-      freeloader.a.must_equal 'bar'
-      freeloader.b.must_equal 'foo'
-      freeloader.z.must_equal 'Alice & Bob'
+      _(freeloader.a).must_equal 'bar'
+      _(freeloader.b).must_equal 'foo'
+      _(freeloader.z).must_equal 'Alice & Bob'
     end
 
     it 'mustn\'t be broken by other\'s magic' do
       performer = Muggle.new.extend(British)
-      performer.perform.must_equal 'Voilà! A rabbit from a top hat!'
+      _(performer.perform).must_equal 'Voilà! A rabbit from a top hat!'
     end
 
     it 'mustn\'t be broken by singleton method' do
@@ -289,15 +289,15 @@ describe 'British module' do
         end
       end
 
-      freeloader.whatever.must_equal 'Hello there! I feel magic!'
-      freeloader.colour.must_equal 'red'
+      _(freeloader.whatever).must_equal 'Hello there! I feel magic!'
+      _(freeloader.colour).must_equal 'red'
     end
 
     it 'must throw NoMethodError on unknown methods' do
       freeloader = Freeloader.new('foo', 'Hello World!')
       freeloader.extend(British)
 
-      -> { freeloader.glamour }.must_raise NoMethodError
+      _(-> { freeloader.glamour }).must_raise NoMethodError
     end
 
     it 'must correctly show nil in NoMethodError messages' do
@@ -307,9 +307,9 @@ describe 'British module' do
         n.nonsense
       rescue NoMethodError => e
         if RUBY_VERSION == '2.0.0'
-          e.message.must_equal 'private method `include\' called for NilClass:Class'
+          _(e.message).must_equal 'private method `include\' called for NilClass:Class'
         else
-          e.message.must_equal 'undefined method `nonsense\' for nil:NilClass'
+          _(e.message).must_equal 'undefined method `nonsense\' for nil:NilClass'
         end
       end
     end
